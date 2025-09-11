b9d7bf5eb798bf5ba67ae436c109c59486cad6d0
Hero-mario <devsunit18@gmail.com>
2025-09-11 10:57:32 +0530

    mm

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Layerflex5.0",   // 👈 case-sensitive repo name
+  base: "./Layerflex5.0",   // 👈 case-sensitive repo name
 })
 
 
